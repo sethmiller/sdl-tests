9fef24f831da5954dc13711ac83da5b6c9b60355
Seth Miller <miller.seth@gmail.com>
2025-01-29 22:30:39 -0600

    raycaster: textured
    See https://lodev.org/cgtutor/raycasting.html

--- raycaster/main.go
+++ raycaster/main.go
@@ -21,12 +21,14 @@ const (
 )
 
 const (
-	width     = 1080
-	height    = 720
-	mapWidth  = 24
-	mapHeight = 24
-	fontPath  = "../assets/fonts/dogica.ttf"
-	fontSize  = 24
+	width         = 1080
+	height        = 720
+	mapWidth      = 24
+	mapHeight     = 24
+	fontPath      = "../assets/fonts/dogica.ttf"
+	fontSize      = 24
+	textureWidth  = 64
+	textureHeight = 64
 )
 
 const halfPi = math.Pi * .5
@@ -44,35 +46,69 @@ func pointp(X int32, Y int32) *sdl.Point {
 }
 
 var worldMap = [mapWidth][mapHeight]int{
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7},
+	{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+	{4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+	{4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+	{4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+	{4, 0, 4, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7},
+	{4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+	{4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
+	{4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1},
+	{4, 0, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
+	{4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+	{4, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 0, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+	{6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 6, 0, 6, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3},
+	{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+	{4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+	{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+	{4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2},
+	{4, 0, 0, 5, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+	{4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+	{4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+	{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3},
+}
+
+func generateTextures(width int32, height int32) *[8][]int32 {
+	textures := [8][]int32{}
+	for i := range textures {
+		textures[i] = make([]int32, width*height)
+	}
+	//generate some textures
+	for x := int32(0); x < width; x++ {
+		for y := int32(0); y < height; y++ {
+			xorcolor := (x * 256 / width) ^ (y * 256 / height)
+			ycolor := y * 256 / height
+			xycolor := y*128/height + x*128/width
+			cross := int32(0)
+			if x != y && x != width-y {
+				cross = 1
+			}
+			redGradient := int32(0)
+			if x%16 != 0 && y%16 != 0 {
+				redGradient = 1
+			}
+			textures[0][width*y+x] = 65536 * 254 * cross                      // flat red texture with black cross
+			textures[1][width*y+x] = xycolor + 256*xycolor + 65536*xycolor    // sloped greyscale
+			textures[2][width*y+x] = 256*xycolor + 65536*xycolor              // sloped yellow gradient
+			textures[3][width*y+x] = xorcolor + 256*xorcolor + 65536*xorcolor // xor greyscale
+			textures[4][width*y+x] = 256 * xorcolor                           // xor green
+			textures[5][width*y+x] = 65536 * 192 * redGradient                // red bricks
+			textures[6][width*y+x] = 65536 * ycolor                           // red gradient
+			textures[7][width*y+x] = 128 + 256*128 + 65536*128                // flat grey texture
+		}
+	}
+
+	return &textures
 }
 
 func run() (err error) {
 	var window *sdl.Window
 	var renderer *sdl.Renderer
+	var surface *sdl.Surface
 	var font *ttf.Font
 
 	if err = ttf.Init(); err != nil {
@@ -95,6 +131,11 @@ func run() (err error) {
 	}
 	defer renderer.Destroy()
 
+	if surface, err = window.GetSurface(); err != nil {
+		return
+	}
+	defer surface.Free()
+
 	// Load the font for our text
 	if font, err = ttf.OpenFont(fontPath, fontSize); err != nil {
 		return
@@ -106,7 +147,7 @@ func run() (err error) {
 
 	// game state
 	posX := 22.0
-	posY := 12.0 // x and y start position
+	posY := 11.5 // x and y start position
 
 	dirX := -1.0
 	dirY := 0.0 // initial direction vector
@@ -115,16 +156,26 @@ func run() (err error) {
 	planeY := 0.66 // the 2d raycaster version of camera plane
 
 	time := sdl.GetTicks64() // time of current frame
-	oldTime := time - 1      // time of previous frame
+	oldTime := time          // time of previous frame
 
+	// Which keys are currently being pressed
 	keys := map[Direction]interface{}{}
 
-	for running {
-		renderer.SetDrawColor(sky.R, sky.G, sky.B, sky.A)
-		renderer.Clear()
+	textures := generateTextures(textureWidth, textureHeight)
+	pixels := surface.Pixels()
 
-		renderer.SetDrawColor(black.R, black.G, black.B, black.A)
-		renderer.FillRect(&sdl.Rect{X: 0, Y: height / 2, W: width, H: height})
+	for running {
+		surface.FillRect(&sdl.Rect{X: 0, Y: 0, W: width, H: height}, 0)
+
+		for x := 0; x < 8*textureWidth; x++ {
+			for y := 0; y < textureHeight; y++ {
+				color := textures[x/textureWidth][textureWidth*y+(x%textureWidth)]
+				i := int32(y)*surface.Pitch + int32(256+x)*int32(surface.Format.BytesPerPixel)
+				pixels[i] = byte(color >> 0 & 0xff)    // b
+				pixels[i+1] = byte(color >> 8 & 0xff)  // g
+				pixels[i+2] = byte(color >> 16 & 0xff) // r
+			}
+		}
 
 		for x := int32(0); x < width; x++ {
 			// calculate ray position and direction
@@ -216,38 +267,65 @@ func run() (err error) {
 				drawEnd = height - 1
 			}
 
-			var color sdl.Color
-			switch worldMap[mapX][mapY] {
-			case 1:
-				color = red
-			case 2:
-				color = green
-			case 3:
-				color = blue
-			case 4:
-				color = white
-			default:
-				color = yellow
+			// texturing calculations
+			texNum := worldMap[mapX][mapY] - 1 // 1 subtracted from it so that texture 0 can be used!
+
+			wallX := 0.0 // where exactly the wall was hit
+			if side == 0 {
+				wallX = posY + perpWallDist*rayDirY
+			} else {
+				wallX = posX + perpWallDist*rayDirX
+			}
+			wallX -= math.Floor(wallX)
+
+			// x coordinate on the texture
+			texX := int32(wallX * float64(textureWidth))
+			if side == 0 && rayDirX > 0 {
+				texX = textureWidth - texX - 1
+			}
+			if side == 1 && rayDirY < 0 {
+				texX = textureWidth - texX - 1
 			}
 
-			// give x and y sides different brightness
-			if side == 1 {
-				color = sdl.Color{R: color.R / 2, G: color.G / 2, B: color.B / 2, A: color.A / 2}
+			step := float64(textureHeight) / float64(lineHeight)
+
+			// Starting texture coordinate
+			texPos := float64(drawStart-height/2+lineHeight/2) * step
+
+			for y := drawStart; y < drawEnd; y++ {
+				// Cast the texture coordinate to integer, and mask with (textureHeight - 1) in case of overflow
+				texY := int32(texPos) & (textureHeight - 1)
+				texPos += step
+				color := textures[texNum][textureHeight*texY+texX]
+				// make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
+				if side == 1 {
+					color = (color >> 1) & 8355711
+				}
+				i := int32(y)*surface.Pitch + int32(x)*int32(surface.Format.BytesPerPixel)
+				pixels[i] = byte(color >> 0 & 0xff)    // b
+				pixels[i+1] = byte(color >> 8 & 0xff)  // g
+				pixels[i+2] = byte(color >> 16 & 0xff) // r
 			}
 
 			// draw the pixels of the stripe as a vertical line
-			renderer.SetDrawColor(color.R, color.G, color.B, color.A)
-			renderer.DrawLine(x, drawStart, x, drawEnd)
+			// renderer.SetDrawColor(color.R, color.G, color.B, color.A)
+			// renderer.DrawLine(x, drawStart, x, drawEnd)
 		}
 
 		// timing for input and FPS counter
-		oldTime = time - 1
+		oldTime = time
 		time = sdl.GetTicks64()
 		frameTime := float64(time-oldTime) / 1000.0 // frameTime is the time this frame has taken, in milliseconds
+
+		surfaceTexture, _ := renderer.CreateTextureFromSurface(surface)
+		renderer.Copy(surfaceTexture, nil, &sdl.Rect{X: 1, Y: 1, W: width, H: height})
+		surfaceTexture.Destroy()
 
 		fps, _ := font.RenderUTF8Blended(fmt.Sprintf("%.0f", (1.0/float64(frameTime))), white)
 		fpsTexture, _ := renderer.CreateTextureFromSurface(fps)
 		renderer.Copy(fpsTexture, nil, &sdl.Rect{X: 2, Y: 2, W: fps.W, H: fps.H})
+		fpsTexture.Destroy()
+		fps.Free()
 
 		moveSpeed := float64(frameTime) * 5.0 // the constant value is in squares/second
 		rotSpeed := float64(frameTime) * 3.0  // the constant value is in radians/second
